10eae452670e2d42febbccc00ae875b59c567ef1
jaedeok95 <dlwoejr700@gmail.com>
2021-07-14 20:41:25 +0900

    Add the author's relationship with the book

--- spring-boot-mysql/src/main/java/com/convenant/springbootmysql/model/Book.java
+++ spring-boot-mysql/src/main/java/com/convenant/springbootmysql/model/Book.java
@@ -1,6 +1,8 @@
 package com.convenant.springbootmysql.model;
 
 import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,4 +17,11 @@ public class Book {
     private Long id;
     private String name;
     private String isbn;
+
+    // 저자와 도서는 1:N관계이므로 한 저자가 여러 책을 쓸 수 있음
+    // 물론 여러 저자가 하나의 책을 쓸 수 있지만 이번 예제에서는 배제함
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    @JsonManagedReference
+    private  Author author;
 }
