6e65827522ace8a8328364d8c53b2137acaf591a
jaedeok95 <dlwoejr700@gmail.com>
2021-07-14 19:46:19 +0900

    Add Author class

--- spring-boot-mysql/src/main/java/com/convenant/springbootmysql/model/MemberStatus.java
+++ spring-boot-mysql/src/main/java/com/convenant/springbootmysql/model/MemberStatus.java
@@ -1,5 +1,9 @@
 package com.convenant.springbootmysql.model;
 
 public enum MemberStatus {
+    /*
+    * Member 모델의 status 필드에 들어갈 MemberStatus enum 타입은
+    * 회원 활성회원, 비활성 회원으로 구분한다.
+    * */
     ACTIVE, DEACTIVATED
 }
